bd3d3b0fdbcb712c5e44fe60a07a69a1016fc3e6
Dennis <denniseh7@gmail.com>
2018-02-02 02:36:21 -0800

    Twindom_Application Repository Created

---
+++ Twindom_Apply.py
@@ -0,0 +1,27 @@
+#Dennis Hsu
+#Twindom Application Code Review
+#February 1st, 2018
+
+set_1=set([1,2,3])
+vec_1=[3,2,1]
+
+#does not include negative integer testing
+def yyy(x):
+    l=len(x)
+    for i in range(0,l):
+        set_1=set([])
+        for j in range (0,l):
+            if i==j:
+                continue
+            if (x[j]*x[j]) in set_1:
+                print(set_1)
+                return True
+            else:
+                set_1.add(x[i]*x[i]-x[j]*x[j])
+    print(set_1)
+    return False
+
+print(yyy([4,5,3]))
+
+
+print("Hello World")
